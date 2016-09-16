efb7a906825f9e9d34a00fdd3f586f7ef17796cd
Twigen <twigen.zlotooki@gmail.com>
2016-09-16 14:40:17 +0200

    z

---
+++ EFv2/EFv2/PersonDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFv2
+{
+    class PersonDbContext : DbContext
+    {
+        public PersonDbContext() : base("name=PersonContext")
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ EFv2/EFv2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PerformDatabaseOperations();
+            Console.Write("Person saved !");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        ///     The perform database operations.
+        /// </summary>
+        public static void PerformDatabaseOperations()
+        {
+            using (var db = new PersonDbContext())
+            {
+                var person = new Person
+                {
+                    FirstName = "Nadege",
+                    LastName = "Deroussen",
+                    BirthDate = DateTime.Now
+                };
+
+                db.Persons.Add(person);
+                db.SaveChanges();
+            }
+        }
+    
+    }
+}
